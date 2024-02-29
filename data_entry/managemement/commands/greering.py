94eb084e0d71971b8be35832035b1d3f4d071e45
mandeepraut <mandeepraut97@gmail.com>
2024-02-29 21:00:39 +0545

    first commit

---
+++ data_entry/managemement/commands/greering.py
@@ -0,0 +1,6 @@
+from django.core.management.base import  BaseCommand
+class Command(BaseCommand):
+    help="Greets the user"
+
+    def handle(self,*args,**kwargs):
+        self.stdout.write('Hi Mandy Good Morning ')
